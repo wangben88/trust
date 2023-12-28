1fd300f75f78c14fd231e91de992be76dd5a3ea6
Benjie Wang <benjiewang@Benjies-MBP.lan>
2023-12-28 09:17:41 -0800

    add explicit c++11 compile arg

--- build.py
+++ build.py
@@ -4,7 +4,11 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 def build(setup_kwargs):
     ext_modules = [
-        Pybind11Extension("LeafScore", ["trust/leaf_scores/LeafScore.cpp"]),
+        Pybind11Extension("LeafScore", 
+                        ["trust/leaf_scores/LeafScore.cpp"],
+                        extra_compile_args=['-std=c++11']
+        ),
+
     ]
     setup_kwargs.update({
         "ext_modules": ext_modules,
